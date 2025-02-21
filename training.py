1c138f79621e91837181eadc47daf0799b7d9a77
Imtiaz Chowdhury <chowdhury.imtiaz@gmail.com>
2025-02-20 16:57:25 -0800

    Add a training file

--- training.py
+++ training.py
@@ -1,6 +1,10 @@
-
-
 # Training a data set
 
-
+from transformers import AutoTokenizer
 from datasets import load_dataset
+
+# Load the IMDB data set
+dataset = load_dataset("imdb")
+
+# Preprocess the data
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
